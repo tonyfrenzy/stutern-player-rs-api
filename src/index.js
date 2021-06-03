df3cc6e9d83b81c4fb9d5570149b778fdf0fb660
tonyfrenzy <tonyfrenzy@gmail.com>
2021-06-03 13:33:19 +0100

    create server at port:3000

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server connected at  http://localhost:${port}`);
+});
